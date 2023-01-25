26355a08e26f2442a0f6e354466c020581b9e7a7
rajneesh304 <singhatiya.rajneesh2@gmail.com>
2023-01-25 13:38:32 +0530

    beautified UI

--- app.js
+++ app.js
@@ -71,6 +71,10 @@ app.set(express.static(path.join(__dirname, 'public')));
 
 app.engine('ejs', ejsMate);
 
+app.get('/', (req, res) => {
+    res.render('home');
+});
+
 app.all('*', (req, res, next) => {
     next(new ExpressError('Page not found', 404));
 });
